14bb89de63fa43947a6cce512b490029196d9206
Erkin <ematkaziev@optimal-dynamics.com>
2018-07-01 15:49:49 +0600

    use google code style

--- Easy/771.cpp
+++ Easy/771.cpp
@@ -1,17 +1,12 @@
-class Solution
-{
-  public:
-    int numJewelsInStones(string J, string S)
-    {
-        int sum_jewels = 0;
-        for (int i = 0; i < J.length(); i++)
-        {
-            for (int j = 0; j < S.length(); j++)
-            {
-                if (J[i] == S[j])
-                    sum_jewels++;
-            }
-        }
-        return sum_jewels;
+class Solution {
+ public:
+  int numJewelsInStones(string J, string S) {
+    int sum_jewels = 0;
+    for (int i = 0; i < J.length(); i++) {
+      for (int j = 0; j < S.length(); j++) {
+        if (J[i] == S[j]) sum_jewels++;
+      }
     }
+    return sum_jewels;
+  }
 };
