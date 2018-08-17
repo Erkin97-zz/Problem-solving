5672885c84d4a0457d18c768a1efb1bc9ddbfa3b
erkin97 <matkaziev.97@gmail.com>
2018-08-17 15:33:00 +0600

    ez design of hash table with JS

---
+++ Design/706.js
@@ -0,0 +1,46 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function() {
+  this.hs_table = [];
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+  this.hs_table[key] = value;
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+  if (this.hs_table[key] == undefined) {
+    return -1;
+  } else {
+    return this.hs_table[key];
+  }
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+  this.hs_table[key] = undefined;
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = Object.create(MyHashMap).createNew()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
