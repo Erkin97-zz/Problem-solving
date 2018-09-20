7c3971c958a30a3b87fd8fad8a776f2f20ddf1fe
erkin97 <matkaziev.97@gmail.com>
2018-09-20 22:03:51 +0900

    solve 905 space O(N)

---
+++ Arrays/905.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+      vector <int> ans;
+      for (int i = 0; i < A.size(); i++) {
+          if (A[i] % 2 == 0) {
+              ans.insert(ans.begin(), A[i]);
+          } else {
+              ans.push_back(A[i]);
+          }
+      }
+      return ans;
+    }
+};
