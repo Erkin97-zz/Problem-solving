723b3006d430a64b9e89b349838c0d0139919477
erkin97 <matkaziev.97@gmail.com>
2018-09-20 22:23:55 +0900

    solve 905 memory O(1)

--- Arrays/905.cpp
+++ Arrays/905.cpp
@@ -1,16 +1,33 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& A) {
-      vector <int> ans;
-      for (int i = 0; i < A.size(); i++) {
-          if (A[i] % 2 == 0) {
-              ans.insert(ans.begin(), A[i]);
-          } else {
-              ans.push_back(A[i]);
-          }
+    static vector<int> sortArrayByParity(vector<int>& A) {
+      int left = 0;
+      int right = A.size() - 1;
+      while(left < right) {
+        if (A[left] % 2 != 0 && A[right] % 2 == 0) {
+          swap(A[left], A[right]);
+        }
+        if (A[left] % 2 == 0) left++;
+        if (A[right] % 2 != 0) right--;
       }
-      return ans;
+      return A;
     }
 };
+// this part is for testing
+int main () {
+  int n;
+  vector <int> test;
+  cin >> n;
+  test.resize(n);
+  for (int i = 0; i < n; i++) {
+    cin >> test[i];
+  }
+  test = Solution::sortArrayByParity(test);
+  for (int i = 0; i < n; i++) {
+    cout << test[i] << " ";
+  }
+  cout << endl;
+}
