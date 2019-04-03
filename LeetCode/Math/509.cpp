82e3c0b35672241182a0eb83a89123698b402961
(학생) ERKIN MATKAZIEV (전기전자컴퓨터공학부) <erkin97@unist.ac.kr>
2019-04-03 13:37:18 +0900

    Fibonacci Number

---
+++ LeetCode/Math/509.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int fib(int N) {
+    int a, b, c;
+    a = 0, b = 1;
+    if (N == 0) return a;
+    if (N == 1) return b;
+    while (N-- != 1) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return c;
+  }
+};
