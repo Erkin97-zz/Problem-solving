8bea598f3f7c108c3b6e9351ba4bd02bbc40f6fd
(학생) ERKIN MATKAZIEV (전기전자컴퓨터공학부) <erkin97@unist.ac.kr>
2019-06-08 23:59:07 +0900

    Height Checker

--- Hackerrank/Strings/Super Reduced String.cpp
+++ Hackerrank/Strings/Super Reduced String.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 // Complete the superReducedString function below.
 string superReducedString(string s) {
-  if (s == "")
-    return s;
+  if (s == "") return s;
 
   stack<char> st;
 
---
+++ LeetCode/Arrays/1051.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int sum = 0;
+        vector <int> h;
+        h = heights;
+        sort(heights.begin(), heights.end());
+        for (int i = 0; i < heights.size(); i++) {
+            sum += (heights[i] != h[i]);
+        }
+        return sum;
+    }
+};
