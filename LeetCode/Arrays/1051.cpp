57971edcc9e9de3fb1cb6a7ce430b158629bfd9e
(학생) ERKIN MATKAZIEV (전기전자컴퓨터공학부) <erkin97@unist.ac.kr>
2019-06-08 23:59:19 +0900

    f

--- LeetCode/Arrays/1051.cpp
+++ LeetCode/Arrays/1051.cpp
@@ -1,13 +1,13 @@
 class Solution {
-public:
-    int heightChecker(vector<int>& heights) {
-        int sum = 0;
-        vector <int> h;
-        h = heights;
-        sort(heights.begin(), heights.end());
-        for (int i = 0; i < heights.size(); i++) {
-            sum += (heights[i] != h[i]);
-        }
-        return sum;
+ public:
+  int heightChecker(vector<int>& heights) {
+    int sum = 0;
+    vector<int> h;
+    h = heights;
+    sort(heights.begin(), heights.end());
+    for (int i = 0; i < heights.size(); i++) {
+      sum += (heights[i] != h[i]);
     }
+    return sum;
+  }
 };
